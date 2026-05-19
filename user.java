0a211b3762d5c4c367d33f60a18d72db2534361b
NailaZahra <nailazahray@gmail.com>
2026-05-19 19:30:42 +0700

    tambah class User dengan field, constructor, dan validasiLogin

--- user.java
+++ user.java
@@ -1,3 +1,33 @@
-public class user {
+public class user implements Tampilan {
+    private String username;
+    private String password;
+
+    public user(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        if (password == null || password.isEmpty()) {
+            throw new IllegalArgumentException("Password tidak boleh kosong.");
+        }
+
+        this.password = password;
+    }
+
+    public boolean validasiLogin(String username, String password) {
+        if (username == null || password == null) return false;
+        return this.username.equals(username) && this.password.equals(password);
+    }
+
     
+    @Override
+    public String tampilkanInfo() {
+        return "User: " + username;
+    }
 }
+
