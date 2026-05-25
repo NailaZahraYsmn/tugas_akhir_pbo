06e4664ec6eed15ffc17c3192920e9d72966a14f
azzamaulidya <azzamaulidya07@gmail.com>
2026-05-25 19:51:39 +0700

    Membuat struktur Controller

---
+++ JabatanController.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class JabatanController {
+
+    private ArrayList<Jabatan> daftarJabatan;
+
+    public JabatanController() {
+        daftarJabatan = new ArrayList<>();
+    }
+}
---
+++ KaryawanController.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class KaryawanController {
+
+    private ArrayList<KaryawanAktif> daftarAktif;
+    private ArrayList<KaryawanNonAktif> daftarNonAktif;
+
+    public KaryawanController() {
+        daftarAktif = new ArrayList<>();
+        daftarNonAktif = new ArrayList<>();
+    }
+
+
+}
