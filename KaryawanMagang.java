d32ac29dceb844c2b229874f04cfb7b4df54b3e7
NailaZahra <nailazahray@gmail.com>
2026-06-09 14:06:01 +0700

    menambahkan construktor

--- KaryawanMagang.java
+++ KaryawanMagang.java
@@ -1,3 +1,10 @@
 public class KaryawanMagang extends Karyawan {
     private String DurasiMagang; 
+    public KaryawanMagang (String idKaryawan, String nama, Jabatan jabatan, String DurasiMagang) {
+        super(idKaryawan, nama, jabatan);
+        if (DurasiMagang == null || DurasiMagang.isEmpty()) {
+            throw new IllegalArgumentException("Durasi magang tidak boleh kosong.");
+        }
+        this.DurasiMagang = DurasiMagang;
+    }
 }
